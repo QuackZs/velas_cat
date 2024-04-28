9e560ee086e98b010e7c1d43eefe3ca599f00023
QuackZs <marco_marconis@hotmail.com>
2024-04-28 23:38:44 +0100

    update

---
+++ resources/views/new_product.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backoffice</title>
+    <!-- Inclua os arquivos CSS do Bootstrap -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex justify-content-between">
+                    <span>Novo Produto</span>
+                </div>
+                
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
